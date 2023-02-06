4c0e2882a6626824487b82f097be6f069ce7669a
Rohit Jaiswal <rohitjaiswal.rj6585@gmail.com>
2023-02-06 21:46:52 +0530

    pattern printing eg 2 added

---
+++ pattern_printing_2.cpp
@@ -0,0 +1,25 @@
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+#include <bits\stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int i,j;
+
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            (j<=5-i)?cout<<" ":cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
