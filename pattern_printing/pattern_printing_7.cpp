912c923f8a5459a12b906c98589ba5dd567718ca
Rohit Jaiswal <rohitjaiswal.rj6585@gmail.com>
2023-02-07 17:30:07 +0530

    eg 6 tested

---
+++ pattern_printing/pattern_printing_7.cpp
@@ -0,0 +1,31 @@
+// *********
+// **** ****
+// ***   ***
+// **     **
+// *       *
+
+#include <bits\stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1; j<=i+4; j++)
+        {
+            if(j<=5-i)
+                cout<<" ";
+            else
+            {
+                if((i+j)%2==0)
+                    cout<<"*";
+                else
+                    cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
