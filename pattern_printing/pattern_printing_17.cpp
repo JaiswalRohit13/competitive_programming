077a3306ae4a22d30c89c21a1256e09d558e7e06
Rohit Jaiswal <rohitjaiswal.rj6585@gmail.com>
2023-02-15 22:32:16 +0530

    pattern printing eg 17 added

---
+++ pattern_printing/pattern_printing_17.cpp
@@ -0,0 +1,36 @@
+// *********
+// **** ****
+// ***   ***
+// **     **
+// *       *
+// **     **
+// ***   ***
+// **** ****
+// *********
+
+
+#include <bits\stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int i,j;
+
+    for(i=1; i<=9 ;i++)
+    {
+        for(j=1; j<=9 ; j++)
+        {
+            if()
+            {
+                cout<<" ";
+            }
+            else
+            {
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
