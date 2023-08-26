f37b0b79d95023204ed91a3deafb4b3803ff6308
Rohit Jaiswal <rohitjaiswal.rj6585@gmail.com>
2023-08-27 04:02:33 +0530

    bitwise operators program added

---
+++ love babbar/Biwise operators/bitwiseoperators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //biwise and or not and xor
+    int a = 4, b=6;
+
+    cout<< "a & b = "<< (a&b) <<endl;
+    cout<< "a | b = "<< (a|b) <<endl;
+    cout<< "~a = "<< ~a <<endl;
+    cout<< "a ^ b = "<< (a^b) <<endl;
+
+    //left and right shift operator - bitwise operation
+    //padding by 0 in positive number - compiler dependent padding in negative number
+
+    a=10,b=20;
+    cout<<"a<<2 = " << (a<<2) << endl; //left shift = *2 *2 (multiplication stands true for small numbers only)
+    cout<<"b>>2 = " << (b>>3) << endl; //right shift = /2/2/2 (divison stands true for small numbers only)
+
+}
